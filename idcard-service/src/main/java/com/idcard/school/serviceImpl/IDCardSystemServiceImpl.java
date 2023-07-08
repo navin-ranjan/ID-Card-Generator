5db6ac5816ab01f06eb01f2cea015256a46ce5e4
navin-ranjan <navinranjan15498@gmail.com>
2023-07-08 11:55:27 +0530

    pdf implemented done with student pic

--- idcard-service/src/main/java/com/idcard/school/serviceImpl/IDCardSystemServiceImpl.java
+++ idcard-service/src/main/java/com/idcard/school/serviceImpl/IDCardSystemServiceImpl.java
@@ -1,7 +1,9 @@
 package com.idcard.school.serviceImpl;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +16,7 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.PDPageContentStream.AppendMode;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.apache.pdfbox.rendering.PDFRenderer;
 import org.springframework.stereotype.Service;
 
@@ -50,13 +53,6 @@ public class IDCardSystemServiceImpl implements IDCardSystemService{
 	@Override
 	public IDCardWrapper createIdcard(IDCardWrapper idCardWrapper) {
 		logger.info("Insider the createIdcard method with entity value : {} ", idCardWrapper.toString());
-		//code
-		return null;
-	}
-	
-	public static IDCardWrapper createIdcardadd(IDCardWrapper idCardWrapper) {
-		logger.info("Insider the createIdcard method with entity value : {} ", idCardWrapper.toString());
-		
 		try {
 			PDDocument document = PDDocument.load(new File("src/main/java/com/idcard/school/idcard.pdf"));
 
@@ -129,6 +125,9 @@ public class IDCardSystemServiceImpl implements IDCardSystemService{
 	        contentStream.showText(idCardWrapper.getSerialNumber());
 	        contentStream.endText();
 	        
+	        
+	        //contentStream.d
+	        
 //	        contentStream.beginText();
 //	        
 	//
@@ -155,6 +154,150 @@ public class IDCardSystemServiceImpl implements IDCardSystemService{
 	        // Close the document
 	        document.close();
 
+	        System.out.println("Conversion complete.");
+			//code
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return null;
+	}
+	
+	public static IDCardWrapper createIdcardadd(IDCardWrapper idCardWrapper) {
+		logger.info("Insider the createIdcard method with entity value : {} ", idCardWrapper.toString());
+		
+		try {
+			PDDocument document = PDDocument.load(new File("src/main/java/com/idcard/school/idcard.pdf"));
+
+	        // Get the first page
+	        PDPage page = document.getPage(0);
+
+	        // Create a content stream to write on the page
+	        PDPageContentStream contentStream = new PDPageContentStream(document, page,AppendMode.APPEND,true);
+	        
+	        BufferedImage image = ImageIO.read(new File("src/main/java/com/idcard/school/pic.jpg"));
+	        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(image, "jpg", baos);
+            byte[] byteArray = baos.toByteArray();
+	        PDImageXObject imageXObject = PDImageXObject.createFromByteArray(document, byteArray, "image");
+	        
+	        BufferedImage imageSign = ImageIO.read(new File("src/main/java/com/idcard/school/universal.png"));
+
+            // Convert the image to a byte array
+          
+            
+           ByteArrayOutputStream baosSign = new ByteArrayOutputStream();
+            ImageIO.write(imageSign, "png", baosSign);
+           byte[] byteArraysign = baosSign.toByteArray();
+            
+            System.out.println(Arrays.toString(byteArray));
+            System.out.println(Arrays.toString(byteArraysign));
+            
+            
+            PDImageXObject imageXObject1 = PDImageXObject.createFromByteArray(document, byteArraysign, "image");
+
+	        contentStream.setFont(PDType1Font.COURIER_OBLIQUE, 26);
+	        // Modify the content of the page
+	        contentStream.beginText();
+	        contentStream.newLineAtOffset(315,519);
+	        contentStream.showText(idCardWrapper.getFatherName());
+	        contentStream.endText();
+	        
+	        contentStream.beginText();
+	        contentStream.newLineAtOffset(300,469);
+	        contentStream.showText(idCardWrapper.getDob());
+	        contentStream.endText();
+	        
+	        contentStream.beginText();
+	        contentStream.newLineAtOffset(220,425);
+	        contentStream.showText(idCardWrapper.getGender());
+	        contentStream.endText();
+	        
+	        contentStream.beginText();
+	        contentStream.newLineAtOffset(220,385);
+	        contentStream.showText(idCardWrapper.getDegree());
+	        contentStream.endText();
+	        
+	        contentStream.beginText();
+	        contentStream.newLineAtOffset(230,343);
+	        contentStream.showText(idCardWrapper.getSession());
+	        contentStream.endText();
+	        
+	        contentStream.beginText();
+	        contentStream.newLineAtOffset(260, 301);
+	        contentStream.showText(idCardWrapper.getUniversityName());
+	        contentStream.endText();
+	        
+	        	        
+	        contentStream.beginText();
+	        contentStream.newLineAtOffset(210, 260);
+	        contentStream.showText(idCardWrapper.getEmail());
+	        contentStream.endText();
+	        
+	        contentStream.beginText();
+	        contentStream.newLineAtOffset(263, 215);
+	        contentStream.showText(idCardWrapper.getMobile());
+	        contentStream.endText();
+	        
+	        contentStream.beginText();
+	        contentStream.newLineAtOffset(235, 171);
+	        contentStream.showText(idCardWrapper.getAddress());
+	        contentStream.endText();
+	        
+	        
+	        contentStream.drawImage(imageXObject, 850, 400, 130, 150);
+	        contentStream.drawImage(imageXObject1, 800, 400, 125, 40);
+	        
+	        
+	        contentStream.setFont(PDType1Font.COURIER_BOLD, 29);
+	        contentStream.setNonStrokingColor(0.800f, 0.800f,0.800f);
+	        contentStream.beginText();
+	        contentStream.newLineAtOffset(30,22);
+	        contentStream.showText(idCardWrapper.getCollegeName());
+	        contentStream.endText();
+	        
+	        contentStream.setFont(PDType1Font.COURIER_BOLD, 36);
+	        contentStream.setNonStrokingColor(0.200f, 0.400f,0.600f);
+	        contentStream.beginText();
+	        contentStream.newLineAtOffset(100, 570);
+	        contentStream.showText(idCardWrapper.getName());
+	        contentStream.endText();
+	        
+	        contentStream.setFont(PDType1Font.COURIER_BOLD, 29);
+	        contentStream.setNonStrokingColor(0.200f, 0.200f,0.300f);
+	        contentStream.beginText();
+	        contentStream.newLineAtOffset(200, 640);
+	        contentStream.showText(idCardWrapper.getSerialNumber());
+	        contentStream.endText();
+	        
+//	        contentStream.beginText();
+//	        
+	//
+//	        contentStream.endText();
+	        
+	        
+
+	        System.out.println(contentStream);
+	        
+	        // Close the content stream
+	        contentStream.close();
+
+	        PDFRenderer pd = new PDFRenderer(document);
+	        //BufferedImage bufferedImage = pd.renderImageWithDPI(0, 100); // 300 DPI
+	        BufferedImage bufferedImage=pd.renderImage(0);
+
+	        // Convert the modified page to a BufferedImage
+	        //BufferedImage bufferedImage = page.convertToImage(BufferedImage.TYPE_INT_RGB, 300);
+	       
+
+	        System.out.println(bufferedImage);
+	        System.out.println(Arrays.toString(byteArray));
+	        // Save the BufferedImage to a file
+	        ImageIO.write(bufferedImage, "png", new File("src/main/java/com/idcard/school/output.png"));
+
+	        // Close the document
+	        document.close();
+	        System.out.println(Arrays.toString(byteArray));
 	        System.out.println("Conversion complete.");
 			//code
 			
